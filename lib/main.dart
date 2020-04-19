1a9f4337d6cd6b14d1755c009a41f3a52ddf2265
aalberti333 <aalberti333@protonmail.com>
2020-04-19 13:25:22 -0600

    routing edits

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,11 @@ class MyApp extends StatelessWidget {
               body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
               body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
               title: TextStyle(fontSize: 20, fontFamily: 'RobotoCondensed', fontWeight: FontWeight.bold))),
-      home: CategoriesScreen(),
+      //home: CategoriesScreen(),
+      initialRoute: '/',
       routes: {
-        '/category-meals': (ctx) => CategoryMealsScreen(), 
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(), 
       },
     );
   }
